1166df69c181350572b5a74073226361c2530b8d
Sachin saha <sachinsaha628@gmail.com>
2025-08-21 21:58:55 +0530

    my portfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -96,7 +96,7 @@ export const portfolioData = {
     {
       id: 1,
       degree: "Bachelor of Technology in Computer Science & Engineering",
-      institution: "JIT (Jhulelal Institute of Technology,Koradi Nagpur)",
+      institution: "JIT (Jhulelal Institute of Technology)",
       duration: "2022 - 2026",
       status: "Graduating in May 2026",
       gpa: "8.2/10",
